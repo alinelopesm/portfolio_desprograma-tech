b783c3405d2b9a895ce6094b860cd0fb58c2c500
alinelopesm <alinealopesm@gmail.com>
2023-11-08 10:00:06 -0300

    refactor: add space before function

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: 'Portifólio Desprograma Tech',
 }
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode
