f6310ffd8495928f0759a134c89020cc1c330bde
alinelopesm <alinealopesm@gmail.com>
2023-11-08 16:48:27 -0300

    feat: add component layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Layout from '@/components/layout/index';
+import { Box, Typography, Button } from '@mui/material';
 
 const App: React.FC = () => {
   return (
-    <>Nova estrutura</>
+    <Layout> 
+      <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+    >
+      <Typography variant="h4" gutterBottom>
+        Boas-vindas ao meu portfolio!!
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        Eu sou Aline Lopes
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        Desenvolvedora Web & UX/UI Designer
+      </Typography>
+      <Typography variant="body1">
+        AQUI VAI UMA BREVE DESCRIÇÃO OU SLOGAN
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Veja alguns lugares por onde passei!
+      </Typography>
+      <div className='flex flex-row gap-4'>
+        <Button variant="contained" color="primary" href="/seu-curriculo">
+          Currículo
+        </Button>
+        <Button variant="contained" color="primary" href="/contato">
+          Contato
+        </Button>
+      </div>
+    </Box>
+    </Layout>
   );
 };
 
